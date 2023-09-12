c696a3fc8c70fe5597d60dbe7e3628be4cec6dd5
Jakub Gerec <jakub.gerec@student.tuke.sk>
2023-09-12 03:40:59 +0200

    benchmarks update

--- src/benchmark.go
+++ src/benchmark.go
@@ -2,53 +2,70 @@ package src
 
 import (
 	"fmt"
-	"time"
 	"math/rand"
+	"time"
 )
 
 const (
-	GAMES = 10
-	MINIMAX_DEPTH = 2
+	GAMES           = 10
+	MINIMAX_DEPTH   = 2
+	MCTS_ITERATIONS = 100
+
 	MAGENTA_ANSI = "\x1b[35m"
-	BLUE_ANSI = "\x1b[34m"
+	BLUE_ANSI    = "\x1b[34m"
 )
 
 func RunBenchmarks() {
 	fmt.Printf("%sRunning benchmarks...%s\n", MAGENTA_ANSI, RESET_ANSI)
 	start_time := time.Now()
 
-	fmt.Printf("%sRandom vs. Random%s\n", BLUE_ANSI, RESET_ANSI)
+	fmt.Printf("%s\nRandom vs Random%s\n", BLUE_ANSI, RESET_ANSI)
 	benchmarkGame(random_move, random_move, 0, 0)
 
-	fmt.Printf("%sBLACK Minimax vs. WHITE Random%s\n", BLUE_ANSI, RESET_ANSI)
+	fmt.Printf("%s\nBLACK Minimax vs WHITE Random%s\n", BLUE_ANSI, RESET_ANSI)
 	benchmarkGame(MinimaxMove, random_move, MINIMAX_DEPTH, 0)
 
-	fmt.Printf("%sWHITE Minimax vs. BLACK Minimax%s\n", BLUE_ANSI, RESET_ANSI)
+	fmt.Printf("%s\nWHITE Minimax vs BLACK Random%s\n", BLUE_ANSI, RESET_ANSI)
 	benchmarkGame(random_move, MinimaxMove, 0, MINIMAX_DEPTH)
 
-	fmt.Printf("%sMinimax vs. Minimax%s\n", BLUE_ANSI, RESET_ANSI)
+	fmt.Printf("%s\nMinimax vs Minimax%s\n", BLUE_ANSI, RESET_ANSI)
 	benchmarkGame(MinimaxMove, MinimaxMove, MINIMAX_DEPTH, MINIMAX_DEPTH)
 
-	end_time := time.Since(start_time)
-	fmt.Printf("Benchmarks completed in %s\n", end_time)
+	// fmt.Printf("%s\nBLACK MCTS vs WHITE Random%s\n", BLUE_ANSI, RESET_ANSI)
+	// benchmarkGame(MctsMove, random_move, MCTS_ITERATIONS, 0)
+
+	// fmt.Printf("%s\nWHITE MCTS vs BLACK Random%s\n", BLUE_ANSI, RESET_ANSI)
+	// benchmarkGame(random_move, MctsMove, 0, MCTS_ITERATIONS)
+
+	// fmt.Printf("%s\nMCTS vs MCTS%s\n", BLUE_ANSI, RESET_ANSI)
+	// benchmarkGame(MctsMove, MctsMove, MCTS_ITERATIONS, MCTS_ITERATIONS)
+
+	// fmt.Printf("%s\nBLACK Minimax vs WHITE MCTS%s\n", BLUE_ANSI, RESET_ANSI)
+	// benchmarkGame(MinimaxMove, MctsMove, MINIMAX_DEPTH, MCTS_ITERATIONS)
+
+	// fmt.Printf("%s\nBLACK MCTS vs WHITE Minimax%s\n", BLUE_ANSI, RESET_ANSI)
+	// benchmarkGame(MctsMove, MinimaxMove, MCTS_ITERATIONS, MINIMAX_DEPTH)
+
+	end_time := time.Since(start_time).Seconds()
+	fmt.Printf("\nTotal elapsed time: %.2f\n", end_time)
 }
 
 func benchmarkGame(
-    black_ai func(Othello, int) [2]int,
-    white_ai func(Othello, int) [2]int,
-    black_iterations int,
-    white_iterations int,
+	black_ai func(Othello, int) [2]int,
+	white_ai func(Othello, int) [2]int,
+	black_iterations int,
+	white_iterations int,
 ) {
-    var black_wins int
-    var white_wins int
-    var draws int
+	var black_wins int
+	var white_wins int
+	var draws int
 
-    start := time.Now()
-    for i := 0; i < GAMES; i++ {
-        fmt.Printf("  game: %d/%d, elapsed time: %.2fs\r", i+1, GAMES, time.Since(start).Seconds())
+	start := time.Now()
+	for i := 0; i < GAMES; i++ {
+		fmt.Printf("  game: %d/%d, elapsed time: %.2fs\r", i+1, GAMES, time.Since(start).Seconds())
 
-        game := NewOthello()
-        for game.state == BLACK_TURN || game.state == WHITE_TURN {
+		game := NewOthello()
+		for game.state == BLACK_TURN || game.state == WHITE_TURN {
 			var move [2]int
 			if game.state == BLACK_TURN {
 				move = black_ai(game, black_iterations)
@@ -56,19 +73,19 @@ func benchmarkGame(
 				move = white_ai(game, white_iterations)
 			}
 			game.MakeMove(move)
-		} 
-        if game.state == BLACK_WON{
-            black_wins++
-        } else if game.state == WHITE_WON {
-            white_wins++
-        } else {
-            draws++
-        }
-    }
-    fmt.Printf("  elapsed time: %.2fs                     \n", time.Since(start).Seconds())
-    fmt.Printf("    BLACK wins: %d %.1f%%\n", black_wins, (float32(black_wins) / float32(GAMES)) * 100)
-    fmt.Printf("    WHITE wins: %d %.1f%%\n", white_wins, (float32(white_wins) / float32(GAMES)) * 100)
-    fmt.Printf("         Draws: %d %.1f%%\n", draws, (float32(draws) / float32(GAMES)) * 100)
+		}
+		if game.state == BLACK_WON {
+			black_wins++
+		} else if game.state == WHITE_WON {
+			white_wins++
+		} else {
+			draws++
+		}
+	}
+	fmt.Printf("  elapsed time: %.2fs                     \n", time.Since(start).Seconds())
+	fmt.Printf("    BLACK wins: %d %.1f%%\n", black_wins, (float32(black_wins)/float32(GAMES))*100)
+	fmt.Printf("    WHITE wins: %d %.1f%%\n", white_wins, (float32(white_wins)/float32(GAMES))*100)
+	fmt.Printf("         Draws: %d %.1f%%\n", draws, (float32(draws)/float32(GAMES))*100)
 }
 
 func random_move(game Othello, iterations int) [2]int {
